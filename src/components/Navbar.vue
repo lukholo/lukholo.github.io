a35df1947ab863dd9629db81f17ea0062f4296b9
Lukholo Mini <lukholo.mini@hotmail.com>
2018-06-02 09:52:04 +0200

    add employee templates

---
+++ src/components/Navbar.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="nav-wrapper">
+    Navbar
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import Dashboard from '@component/Dashboard'
+import NewEmployee from '@component/NewEmployee'
+import EditEmployee from '@component/EditEmployee'
+import ViewEmployee from '@component/ViewEmployee'
 
 Vue.use(Router)
 
@@ -8,8 +12,23 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/new',
+      name: 'new-employee',
+      component: NewEmployee
+    },
+    {
+      path: '/edit',
+      name: 'edit-employee',
+      component: EditEmployee
+    },
+    {
+      path: '/view',
+      name: 'view-employee',
+      component: ViewEmployee
     }
   ]
 })
